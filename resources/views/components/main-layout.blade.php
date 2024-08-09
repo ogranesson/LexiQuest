ff50e99b20303b7ce0249fa588d1e658681f13be
ogranesson <shresthaanshu555@gmail.com>
2024-08-09 15:42:28 +0200

    Layout and welcome test

---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,19 @@
+@props(['title'])
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title ?? "LinguaQuest" }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-white">
+    <div class="bg-white border-b border-b-black h-12 px-2 flex items-center sticky">
+        <p>LinguaQuest</p>
+    </div>
+    <div class="p-3">
+        {{ $slot }} {{-- whatever is pasted between the <x-main-layout> tags is saved as $slot --}}
+    </div>
+</body>
+</html>
