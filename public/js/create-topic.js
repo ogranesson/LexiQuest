89226654dd8bf317d48a0abefa4c878bf71cc951
ogranesson <shresthaanshu555@gmail.com>
2024-08-20 21:04:54 +0200

    bug fix in opiton replacement

--- public/js/create-topic.js
+++ public/js/create-topic.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    var $categoriesList = [];
+    let $categoriesList = [];
     $(document).on('click', '#addCategory' ,function(e) {
         $('#selectMenuDiv').removeClass("hidden");
     });
 
     $(document).on('click', '#addConfirm', function(e) {
         if($('#selectMenu').val() != 0) {
-            var $newCategory = '<div class="category flex row"><div class="text">' + $('#selectMenu option:selected').text() + '</div><button name="delete" class="deleteCategory" type="button" class="px-2">&times;</button></div>';
+            let $newCategory = '<div class="category flex row"><div class="text">' + $('#selectMenu option:selected').text() + '</div><button name="delete" class="deleteCategory" type="button" class="px-2">&times;</button></div>';
             $('#selectedCategories').append($newCategory);
             $categoriesList.push($('#selectMenu').val());
             $("#selectMenu option[value='" + $('#selectMenu').val() + "']").remove();
@@ -16,20 +16,24 @@ $(document).ready(function() {
     });
 
     $(document).on('click', '.deleteCategory', function(e) {
-        var $categories = $('.category'); // Getting all elements with the category class
-        var $categoryText = $(this).closest('.category').find('.text').text().trim(); // Getting the text of the element without the 'x'
+        let $categories = $('.category'); // Getting all elements with the category class
+        let $categoryText = $(this).closest('.category').find('.text').text().trim(); // Getting the text of the element without the 'x'
 
-        var $index = $categories.filter(function() {
+        let $index = $categories.filter(function() {
             return $(this).find('.text').text().trim() === $categoryText;
         }).index(); // Finding the position of the category in the selections
 
-        var $value = $categoriesList[$index]; // value of the category to be closed 
+        let $value = $categoriesList[$index]; // value of the category to be closed 
 
-        var $newOption = '<option value="' + $value + '">' + $categoryText + '</option>';
+        let $newOption = '<option value="' + $value + '">' + $categoryText + '</option>';
 
-        var $select = $('#selectMenu').find('option'); // getting all options from the dropdown
+        let $select = $('#selectMenu').find('option'); // getting all options from the dropdown
 
-        $select.eq($value).before($newOption); // placing the option back
+        let insertIndex = $select.toArray().findIndex(function(option) {
+            return parseInt($(option).val(), 10) > $value; // findIndex returns the index of the first result that satisfies this condition
+        });                                                // in this case, the value being higher than the value of the deleted option
+
+        $($newOption).insertBefore($select.eq(insertIndex));
 
         $categoriesList.splice($index, 1); // deleting it from the categories array
 
